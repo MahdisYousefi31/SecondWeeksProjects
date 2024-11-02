39e535ff52e7df7bc932656a6b444f324687b023
MahdisYousefi31 <mahdis.yousefi84@gmail.com>
2024-11-02 19:20:40 +0330

    Add files via upload

---
+++ NumberExtracter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahdis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter some text (only numbers will be extracted):");
+            string input = Console.ReadLine();
+
+            string numbers = ExtractNumbers(input);
+
+            Console.WriteLine($"Extracted numbers: {numbers}");
+        }
+
+        static string ExtractNumbers(string input)
+        {
+            char[] chars = input.ToCharArray();
+            string result = "";
+
+            foreach (char c in chars)
+            {
+                if (char.IsDigit(c))
+                {
+                    result += c;
+                }
+            }
+            return result;
+        }
+    }
+}
+
